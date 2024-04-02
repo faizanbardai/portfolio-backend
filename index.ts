937e1c209b4cd39d666eaa11a9f5380f15d2b6f5
FayJu <faizan.badruddin.bardai@gmail.com>
2024-04-02 15:22:50 +0200

    Update dependencies and add rate limiting middleware

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
-import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
 import cors from "cors";
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import { rateLimit } from "express-rate-limit";
 import morgan from "morgan";
 
-import shortURLRouter from "./projects/shortURL/routes";
 import { corsSU } from "./projects/shortURL/corsSU";
+import shortURLRouter from "./projects/shortURL/routes";
 
 dotenv.config();
 
@@ -16,6 +17,16 @@ app.use(bodyParser.json());
 
 // morgan
 app.use(morgan("dev"));
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
